5bb525ab3009ef85ba493964de854f043e7bc0fa
HECTORTEMA <69021766+HECTORTEMA@users.noreply.github.com>
2020-08-04 18:19:25 +0300

    Update SaNHoK.lua

--- SaNHoK.lua
+++ SaNHoK.lua
@@ -7966,15 +7966,15 @@ return false
 end
 tdcli_function({ID = "GetChannelMembers",channel_id_ = msg.chat_id_:gsub('-100',''), offset_ = 0,limit_ = 200
 },function(ta,SaNHoK)
-local t = "\nツ قائمة الاعضاء \n≪━━━━━━━━━━━━━≫\n"
+local t = "\n.𖤓 ┆شتاقيتلكم يحلوين 💗◟\n≪━━━━━━━━━━━━━≫\n"
 x = 0
 local list = SaNHoK.members_
 for k, v in pairs(list) do
 x = x + 1
 if database:get(bot_id..'user:Name'..v.user_id_) then
-t = t..""..x.." → {[@"..database:get(bot_id..'user:Name'..v.user_id_).."]}\n"
+t = t..""..x.." → {[@"..database:get(bot_id..'user:Name'..v.user_id_).."]}*◞ 🍭🧚‍♀\n"
 else
-t = t..""..x.." → {"..v.user_id_.."}\n"
+t = t..""..x.." → {"..v.user_id_.."}*◞ 🍭🧚‍♀\n"
 end
 end
 send(msg.chat_id_,msg.id_,t)
